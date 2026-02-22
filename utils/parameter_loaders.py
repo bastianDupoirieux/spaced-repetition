8edc95b747fa7c13a7fb198cfe48dceafdbeaf5f
bastianDupoirieux <bast.dupo@gmail.com>
2026-02-22 14:04:44 +0100

    added method to load parameters by method

---
+++ utils/parameter_loaders.py
@@ -0,0 +1,14 @@
+from enums.methods import SpacedRepetitionMethod
+from typing import Dict, Any
+
+from constants import leitner_method
+
+def load_parameters(method: SpacedRepetitionMethod) -> Dict[str, Any]:
+    match method:
+        case SpacedRepetitionMethod.LEITNER_METHOD:
+            return load_leitner_method_parameters()
+
+def load_leitner_method_parameters() -> Dict[str, Any]:
+    return {
+        "intervals": leitner_method.intervals
+    }
