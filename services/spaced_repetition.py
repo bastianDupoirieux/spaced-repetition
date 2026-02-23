a4db8798ecc153e3d277aa156504c398773a7d75
bastianDupoirieux <bast.dupo@gmail.com>
2026-02-23 19:47:08 +0100

    made start date a variable inside the class to use the same instance for every date

--- services/spaced_repetition.py
+++ services/spaced_repetition.py
@@ -8,21 +8,20 @@ from services.methods.leitner_method import LeitnerMethod
 from utils.parameter_loaders import load_parameters
 
 class SpacedRepetition:
-    def __init__(self, method, start_date:datetime.date):
+    def __init__(self, method):
         self.method = method
-        self.start_date = start_date
         self._ensure_method_is_valid()
 
-    def get_review_dates(self)->List[datetime.date]:
+    def get_review_dates(self, start_date)->List[datetime.date]:
         params= self._load_parameters()
-        method = self.instantiate_method(params)
+        method = self.instantiate_method(params, start_date)
         intervals = method.define_intervals()
         return intervals
 
-    def instantiate_method(self, params):
+    def instantiate_method(self, params, start_date:datetime.date):
         match self.method:
-            case SpacedRepetitionMethod.LEITNER_METHOD:
-                return LeitnerMethod(params, self.start_date)
+            case SpacedRepetitionMethod.LEITNER_METHOD.value:
+                return LeitnerMethod(params, start_date)
 
     def _load_parameters(self):
         params = load_parameters(self.method)
