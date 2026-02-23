40241ad4bc85e94b4eaf2f9e92a2a85aeb8a0aee
bastianDupoirieux <bast.dupo@gmail.com>
2026-02-23 18:26:17 +0100

    added entry point to get all review dates

--- services/spaced_repetition.py
+++ services/spaced_repetition.py
@@ -1,4 +1,5 @@
 import datetime
+from typing import List
 
 from enums.methods import SpacedRepetitionMethod
 
@@ -11,6 +12,12 @@ class SpacedRepetition:
         self.method = method
         self.start_date = start_date
         self._ensure_method_is_valid()
+
+    def get_review_dates(self)->List[datetime.date]:
+        params= self._load_parameters()
+        method = self.instantiate_method(params)
+        intervals = method.define_intervals()
+        return intervals
 
     def instantiate_method(self, params):
         match self.method:
