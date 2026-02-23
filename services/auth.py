b6171ccc5666c1e745f42cdeb0ad77acc777a133
bastianDupoirieux <bast.dupo@gmail.com>
2026-02-23 20:30:13 +0100

    added conversion of credentials to google supported

--- services/auth.py
+++ services/auth.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
 from google_auth_oauthlib.flow import InstalledAppFlow
+from google.oauth2.credentials import Credentials
+import json
 
 from constants.api import google_calendar_scope
 
@@ -8,10 +10,12 @@ load_dotenv()
 
 def load_or_create_token_file():
     if os.path.exists(os.getenv("TOKEN_PATH")):
-        return os.getenv("TOKEN_PATH")
+        return Credentials.from_authorized_user_file(os.getenv("TOKEN_PATH"))
     else:
         flow = InstalledAppFlow.from_client_secrets_file(os.getenv("CREDENTIALS_PATH"), [google_calendar_scope])
         credentials = flow.run_local_server(port=0)
+        credentials.to_json()
         with open(os.getenv("TOKEN_PATH"), "w") as token:
             token.write(credentials.to_json())
-        return os.getenv("TOKEN_PATH")
+        
+        return Credentials.from_authorized_user_file(os.getenv("TOKEN_PATH"))
